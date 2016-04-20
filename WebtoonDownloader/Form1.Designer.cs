92da0f0e69573973033940bc975910387b1a40b0
HP-01 <ajbm1422@naver.com>
2016-04-20 11:06:44 +0900

    Saving

---
+++ WebtoonDownloader/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace WebtoonDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(209, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(47, 21);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "검색";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "노블레스";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(11, 107);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(245, 268);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "웹툰";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(11, 62);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(58, 16);
+            this.checkBox2.TabIndex = 15;
+            this.checkBox2.Text = "HTML";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "시작";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(44, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(51, 20);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "마지막";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(208, 37);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(48, 16);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "전화";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(144, 35);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(53, 20);
+            this.comboBox2.TabIndex = 13;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(191, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 21);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "다운로드";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(138, 59);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(47, 21);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "경로";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(11, 84);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(245, 17);
+            this.progressBar1.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(170, 378);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 12);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Made by ZBM";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 401);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "네이버 웹툰 다운로더";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ WebtoonDownloader/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.ExceptionServices;
+using System.Security;
+using System.Text;
+using System.Threading;
+using System.Web;
+using System.Windows.Forms;
+namespace WebtoonDownloader
+{
+    public partial class Form1 : Form
+    {
+        int n = 0, a = 0, progressbar_max = 0;
+        public static Form1 _frm1;
+        public static Semaphore _resourcePool = null;
+        const int maximum_thread = 20;
+        string name = "", loc = "D:\\";
+        public Form1()
+        {
+            InitializeComponent();
+            _frm1 = this;
+        }
+        public void newLoad(int num)
+        {
+            listBox1.Invoke((Action)delegate
+            {
+                listBox1.Items.Add(num + "화 다운로드 중");
+                listBox1.Update();
+            });
+        }
+        public void doneLoad(int num)
+        {
+            listBox1.Invoke((Action)delegate
+            {
+                listBox1.Items.Remove(num + "화 다운로드 중");
+                listBox1.Update();
+            });
+            progressBar1.Invoke((Action)delegate
+            {
+                progressBar1.Increment(1);
+                if(progressBar1.Value >= progressbar_max && !listBox1.Items.Contains("다운로드 완료!"))
+                {
+                    listBox1.Invoke((Action)delegate
+                    {
+                        listBox1.Items.Add("다운로드 완료!");
+                        listBox1.Update();
+                    });
+                }
+            });
+        }
+        static HtmlElement ElementsByClass(HtmlDocument doc, string className)
+        {
+            foreach (HtmlElement e in doc.All)
+                if (e.GetAttribute("className") == className)
+                    return e;
+            return null;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebBrowser wb = new WebBrowser();
+            wb.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(wb_DocumentCompleted1);
+            wb.AllowNavigation = true;
+            listBox1.Items.Add(textBox1.Text + " 검색 중..");
+            wb.Navigate("http://comic.naver.com/search.nhn?keyword=" + HttpUtility.UrlEncode(textBox1.Text));
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                comboBox1.SelectedIndex = comboBox1.FindStringExact("1");
+                comboBox2.SelectedIndex = comboBox1.FindStringExact(a.ToString());
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            loc = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(n == 0)
+            {
+                MessageBox.Show("웹툰을 조회하세요!");
+                return;
+            }
+            if (checkBox1.Checked)
+            {
+                comboBox1.SelectedIndex = comboBox1.FindStringExact("1");
+                comboBox2.SelectedIndex = comboBox1.FindStringExact(a.ToString());
+            }
+            if(comboBox2.SelectedItem == null || comboBox2.SelectedItem == null)
+            {
+                MessageBox.Show("범위를 선택하세요!");
+                return;
+            }
+            progressbar_max = progressBar1.Maximum = int.Parse(comboBox2.SelectedItem.ToString()) - int.Parse(comboBox1.SelectedItem.ToString());
+            progressBar1.Value = 0;
+            for (int i = int.Parse(comboBox2.SelectedItem.ToString()); i >= int.Parse(comboBox1.SelectedItem.ToString()); i--)
+            {
+                new DownloadTask("http://comic.naver.com/webtoon/detail.nhn?titleId=" + n + "&no=" + i, i, n, name, loc, checkBox2.Checked);
+            }
+            _resourcePool.Release(maximum_thread);
+        }
+
+        private void wb_DocumentCompleted2(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+            WebBrowser wb1 = sender as WebBrowser;
+            HtmlDocument hd = wb1.Document;
+            HtmlElement he = ElementsByClass(hd, "title");
+            if (e.Url.AbsoluteUri != wb1.Url.AbsoluteUri)
+            {
+                return;
+            }
+            if (he == null)
+            {
+                MessageBox.Show("매칭되는게 없음");
+                return;
+            }
+            HtmlElementCollection trs = he.GetElementsByTagName("a");
+            string s = trs[0].GetAttribute("href");
+            if (!s.Contains("webtoon"))
+            {
+                MessageBox.Show("매칭되는게 없음");
+                return;
+            }
+            int tmp = s.IndexOf("no=") + 3;
+            int tmp2 = tmp;
+            while (true)
+            {
+                if ('0' <= s[tmp] && s[tmp] <= '9')
+                {
+                    tmp++;
+                }
+                else break;
+            }
+            a = int.Parse(s.Substring(tmp2, tmp - tmp2));
+            if (_resourcePool == null)
+                _resourcePool = new Semaphore(0, maximum_thread);
+            for (int i = 1; i <= a; i++)
+            {
+                comboBox1.Items.Add(i);
+                comboBox2.Items.Add(i);
+            }
+        }
+
+        private void wb_DocumentCompleted1(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            WebBrowser wb = sender as WebBrowser;
+            HtmlDocument hd = wb.Document;
+            HtmlElement he = ElementsByClass(hd, "resultList");
+            if (e.Url.AbsoluteUri != wb.Url.AbsoluteUri)
+            {
+                return;
+            }
+            listBox1.Items.Add(textBox1.Text + " 검색 성공!");
+            if (he == null)
+            {
+                MessageBox.Show("매칭되는게 없음");
+                return;
+            }
+            HtmlElementCollection trs = he.GetElementsByTagName("a");
+            string s = trs[0].GetAttribute("href");
+            if (!s.Contains("webtoon"))
+            {
+                MessageBox.Show("매칭되는게 없음");
+                return;
+            }
+            name = trs[0].InnerText;
+            MessageBox.Show(trs[0].InnerText+"이(가) 검색되었습니다!");
+            n = int.Parse(s.Substring(s.IndexOf("titleId=") + 8));
+            WebBrowser wb1 = new WebBrowser();
+
+            wb1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(wb_DocumentCompleted2);
+            wb1.Navigate("http://comic.naver.com/webtoon/list.nhn?titleId=" + n);
+        }
+    }
+
+    internal class DownloadTask
+    {
+        private int i;
+        private string v;
+        private int num;
+        private string name;
+        private string loc;
+        private Boolean html_chk = false;
+
+        public DownloadTask(string v, int i, int num, string name, string loc, Boolean html_chk)
+        {
+            this.v = v;
+            this.i = i;
+            this.num = num;
+            this.name = name;
+            this.loc = loc;
+            this.html_chk = html_chk;
+
+            ThreadStart starter = threadJob;
+            starter += () =>
+            {
+                Form1._frm1.doneLoad(i);
+            };
+            Thread thread = new Thread(starter) { IsBackground = true };
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+        }
+        private void threadJob()
+        {
+            Form1._resourcePool.WaitOne();
+            Form1._frm1.newLoad(i);
+            var br = new WebBrowser();
+            br.DocumentCompleted += wb_DocumentCompleted3;
+            br.Navigate(v);
+            Application.Run();
+        }
+        static HtmlElement ElementsByClass(HtmlDocument doc, string className)
+        {
+            foreach (HtmlElement e in doc.All)
+                if (e.GetAttribute("className") == className)
+                    return e;
+            return null;
+        }
+        [HandleProcessCorruptedStateExceptions]
+        [SecurityCritical]
+        private void wb_DocumentCompleted3(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            WebBrowser wb = sender as WebBrowser;
+            HtmlDocument hd = null;
+            try
+            {
+                hd = wb.Document;
+            }
+            catch (Exception e1)
+            {
+                Form1._resourcePool.Release();
+                new DownloadTask(v, i, num, name, loc, html_chk);
+                wb.Dispose();
+                Application.ExitThread();
+                return;
+            }
+            if (hd.Title.Equals("네이버 만화") || hd.Url.Equals("http://comic.naver.com/main.nhn"))
+            {
+                wb.Dispose();
+                Form1._resourcePool.Release();
+                Application.ExitThread();
+                return;
+            }
+            HtmlElement he = ElementsByClass(hd, "wt_viewer");
+            HtmlElementCollection trs = he.GetElementsByTagName("img");
+            if (e.Url.AbsoluteUri != wb.Url.AbsoluteUri)
+            {
+                return;
+            }
+            string html = "<html><head><title> - <" + name + " " + i + "화" + "> - </title></head><body><center>";
+            for (int j = 0; j < trs.Count; j++)
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    if (!System.IO.Directory.Exists(loc + "\\" + name + "\\" + i + "화"))
+                        System.IO.Directory.CreateDirectory(loc + "\\" + name + "\\" + i);
+                    wc.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                    wc.Headers.Add("Referer", "http://comic.naver.com/webtoon/detail.nhn?titleId=" + num + "&no=" + i + "화");
+                    wc.DownloadFile(new System.Uri(trs[j].GetAttribute("src")), loc + "\\" + name + "\\" + i + "\\" + j + ".jpg");
+                }
+                html += "<img src=\"" + i + "\\" + j + ".jpg\">" + "<br>";
+            }
+            html += "</center></body></html>";
+            if (html_chk)
+            {
+                using (FileStream fs = File.Create(loc + "\\" + name + "\\" + i + "화.html"))
+                {
+                    // Add some text to file
+                    Byte[] title = new UnicodeEncoding().GetBytes(html);
+                    fs.Write(title, 0, title.Length);
+                }
+            }
+            wb.Dispose();
+            Form1._resourcePool.Release();
+            Application.ExitThread();
+        }
+    }
+}
